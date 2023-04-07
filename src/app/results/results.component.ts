cc27669391b1415b1bc5c90269d9c97e75c6999c
madhurmehta <madhur.mehta@lotusflare.com>
2023-04-07 19:58:28 -0400

    [feat]  - Add page to show all the scores added  - Add form to add new scores

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {ResultService} from "../http-services/ResultService";
+import {Result} from "./result.model";
+import {SnackBarService} from "../util/SnackBarService";
 
 @Component({
   selector: 'app-results',
@@ -6,5 +9,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./results.component.css']
 })
 export class ResultsComponent {
+
+  results: Result[] = []
+  constructor(private _resultService: ResultService,
+              private _snackBarService: SnackBarService) {}
+
+  ngOnInit() {
+    this.getAllResults();
+  }
+  onRefresh() {
+    this.getAllResults();
+  }
+
+  getAllResults() {
+    this._resultService.getAllResults()
+      .subscribe(
+        response => {
+          console.log(response);
+          this.results = response
+          this._snackBarService.openSnackBar('Loaded All Results', 'success')
+        },
+        err => this._snackBarService.openSnackBar('Failed to load results', 'failed')
+      );
+  }
 
 }
